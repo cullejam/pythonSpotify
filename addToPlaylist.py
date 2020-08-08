cbb3c5844322f01e4e6ce8cedfa6b5bd65ca65b5
cullejam <56239332+cullejam@users.noreply.github.com>
2020-08-08 13:33:37 -0500

    Add files via upload
    Adds a song to the specified playlist. Should later be modified with a loop so that we can add multiple songs quickly.

---
+++ addToPlaylist.py
@@ -0,0 +1,25 @@
+import sys
+
+import spotipy
+import spotipy.util as util
+
+if len(sys.argv) > 3:
+    username = sys.argv[1]
+    playlist_id = sys.argv[2]
+    track_ids = sys.argv[3:]
+else:
+    print("Usage: %s username playlist_id track_id ..." % (sys.argv[0],))
+    sys.exit()
+
+scope = 'playlist-modify-public'
+token = util.prompt_for_user_token(username, scope, client_id='6161afd415714a388660361ac42b170a',
+    client_secret='ffeabedf273a44478e16fdcab38f1693', redirect_uri='http://google.com/')
+
+
+if token:
+    sp = spotipy.Spotify(auth=token)
+    sp.trace = False
+    results = sp.user_playlist_add_tracks(username, playlist_id, track_ids)
+    print(results)
+else:
+    print("Can't get token for", username)
